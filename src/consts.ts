8c0eb64c12385746c64e19728794c7cd50e89472
우재윤 <104556563+iwytbbtss@users.noreply.github.com>
2024-10-29 10:49:00 +0900

    chore: set layouts with tailwind

---
+++ src/consts.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = 'Sunlog';
+export const SITE_DESCRIPTION = '티끌모아 태산같은 블로그가 되어보자';
---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import animate from 'tailwindcss-animate';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Pretendard', defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [animate],
+};
